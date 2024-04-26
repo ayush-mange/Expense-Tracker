1494dd79129cec36685ba2d268b21a04d3bd1f40
Ayush Mange <ayush.mange16743@sakec.ac.in>
2024-04-26 22:01:14 +0530

    UI Update

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useUserAuth } from "../../context/UserAuthContext";
+import closeIcon from '../../Assets/Close 1.png';
 
 const Sidebar = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -29,37 +30,37 @@ const Sidebar = () => {
 >
     {/* Use an image instead of text */}
     {isSidebarOpen ? (
-        <img src="" alt="Close icon" />
+        <img src={require("../../Assets/Close 1.png")} alt="Close icon" className="h-8 w-8 ml-[250px] mt-6"/>
     ) : (
-        <img src="/path_to_menu_icon" alt="Menu icon" />
+        <img src={require("../../Assets/burger menu button.png")} alt="Menu icon" className="h-8 w-8 ml-[25px] mt-4"/>
     )}
 </button>
 
-            <div className={`${isSidebarOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"} bg-[#303030] w-[80%] md:w-[20%] overflow-y-auto h-full fixed text-custom-dark flex flex-col transition-transform duration-300 ease-in-out ${currentPath === "/login" && 'hidden'} ${currentPath === "/register" && 'hidden'}`}>
-                <div className="text-white text-2xl font-semibold justify-center items-center gap-6 ml-[10%] mt-[10%]">
-                    Save Sphere
+            <div className={`${isSidebarOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"} bg-[white] w-[80%] md:w-[20%] overflow-y-auto h-full fixed text-custom-dark flex flex-col transition-transform duration-300 ease-in-out ${currentPath === "/login" && 'hidden'} ${currentPath === "/register" && 'hidden'}`}>
+                <div className="text-#1D1D1D text-2xl font-bold justify-center items-center gap-6 ml-[10%] mt-[10%]">
+                    Expense Tracker
                 </div>
 
-                <div className="mt-[5%] cursor-pointer p-8">
-                    <li className="text-white h-12 flex flex-row items-center gap-6 p-5 rounded-xl font-medium text-base hover:text-lg">
+                <div className="mt-[5%]  cursor-pointer p-8">
+                    <li className="text-#1D1D1D h-12 flex flex-row items-center gap-6 p-5 rounded-xl text-lg font-semibold text-base hover:text-xl font-sans">
                         <Link to="/">Home</Link>
                     </li>
-                    <li className="text-white h-12 flex flex-row items-center gap-6 p-5 rounded-xl font-medium text-base hover:text-lg">
+                    <li className="text-#1D1D1D h-12 flex flex-row items-center gap-6 p-5 rounded-xl text-lg font-semibold text-base hover:text-xl">
                         <Link to="/expense">Expense</Link>
                     </li>
-                    <li className="text-white h-12 flex flex-row items-center gap-6 p-5 rounded-xl font-medium text-base hover:text-lg">
+                    <li className="text-#1D1D1D h-12 flex flex-row items-center gap-6 p-5 rounded-xl text-lg  font-semibold text-base hover:text-xl">
                         <Link to="/income">Income</Link>
                     </li>
                 </div>
 
-                <div className="ml-[10%] mt-[30%] flex flex-col justify-center gap-3 text-white">
-                    <div className="font-medium text-xl cursor-pointer hover:text-[#ccc8c8]">
+                <div className="ml-[20%] mt-[70%] flex flex-col justify-center gap-3 text-#1D1D1D">
+                    <div className="font-medium text-lg font-semibold cursor-pointer hover:text-xl">
                         <Link to="/register">SignUp</Link>
                     </div>
-                    <div className="font-medium text-xl cursor-pointer hover:text-[#ccc8c8]">
+                    <div className="font-medium text-lg font-semibold cursor-pointer hover:text-xl">
                         <Link to="/login">Login</Link>
                     </div>
-                    <div className="font-medium text-xl cursor-pointer hover:text-[#ccc8c8]" onClick={handleLogout}>
+                    <div className="font-medium text-lg font-semibold cursor-pointer hover:text-xl" onClick={handleLogout}>
                         Logout
                     </div>
                 </div>
